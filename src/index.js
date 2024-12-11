06b492a08c86bbb67657281e4b8a354eb1f3ae8e
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2024-12-11 20:45:21 +0300

    add sequelize

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import { Sequelize } from "sequelize";
 
 const app = express();
 const port = 3000;
+
+const sequelize = new Sequelize("scitext", "postgres", "123456", {
+  host: "localhost",
+  dialect: "postgres",
+});
+
+try {
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (e) {
+  console.error("Unable to connect to the database:", e);
+}
 
 app.get("/", (request, response) => {
   response.send("Hello world!");
