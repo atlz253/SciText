3873618f7efd31d76dcccb0f6418f70ac3499d2e
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2024-12-12 23:59:44 +0300

    remove debug message

--- src/parser/worker.js
+++ src/parser/worker.js
@@ -5,10 +5,7 @@ threads.parentPort?.once("message", (buffer) => {
   const chunks = [];
   new PdfReader().parseBuffer(buffer, (err, item) => {
     if (err) threads.parentPort?.postMessage({ error: "parse error" });
-    else if (!item) {
-      console.log("message");
-      threads.parentPort?.postMessage({ text: chunks.join(" ") });
-    }
+    else if (!item) threads.parentPort?.postMessage({ text: chunks.join(" ") });
     else if (item.text) chunks.push(item.text);
   });
 });
