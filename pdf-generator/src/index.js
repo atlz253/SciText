e88fbc6e5034e50a9a66c49a9492e01dd7b7699f
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2024-12-13 21:06:27 +0300

    increased the generation speed using worker threads

--- pdf-generator/src/index.js
+++ pdf-generator/src/index.js
@@ -1,46 +1,79 @@
+import os from "node:os";
 import fs from "node:fs";
 import path from "node:path";
 import PDFDocument from "pdfkit";
+import { fileURLToPath } from "url";
 import { program } from "commander";
 import { faker } from "@faker-js/faker";
+import threads from "node:worker_threads";
 
-const baseOutputDirectory = "./output/pdf-generator";
-const outputDirectory = path.resolve(
-  baseOutputDirectory,
-  new Date().getTime().toString()
-);
+if (threads.isMainThread) {
+  const workerThreads = [];
+  const workerThreadsCount = os.cpus().length;
+  const baseOutputDirectory = "./output/pdf-generator";
+  const outputDirectory = path.resolve(
+    baseOutputDirectory,
+    new Date().getTime().toString()
+  );
+  const __filename = fileURLToPath(import.meta.url);
 
-program
-  .option("--seed <seed>", "random generation seed")
-  // @ts-ignore
-  .option("--count <count>", "pdf files count", 1)
-  // @ts-ignore
-  .option("--min-paragraphs <count>", "min paragraphs in pdf file", 10)
-  // @ts-ignore
-  .option("--max-paragraphs <count>", "max paragraphs in pdf file", 100);
-program.parse(process.argv);
-const options = program.opts();
+  program
+    .option("--seed <seed>", "random generation seed")
+    // @ts-ignore
+    .option("--count <count>", "pdf files count", 1)
+    // @ts-ignore
+    .option("--min-paragraphs <count>", "min paragraphs in pdf file", 10)
+    // @ts-ignore
+    .option("--max-paragraphs <count>", "max paragraphs in pdf file", 100);
+  program.parse(process.argv);
+  const options = program.opts();
 
-fs.mkdirSync(outputDirectory, { recursive: true });
+  fs.mkdirSync(outputDirectory, { recursive: true });
 
-async function generatePDF(fileName) {
-  return new Promise((resolve) => {
-    const doc = new PDFDocument();
-    const stream = fs.createWriteStream(
-      path.resolve(outputDirectory, fileName)
-    );
-    doc.pipe(stream);
-    doc.text(
-      faker.lorem.paragraphs(
-        { min: options.minParagraphs, max: options.maxParagraphs },
-        "\n\n"
-      )
-    );
-    doc.end();
-    stream.on("close", () => resolve(null));
-  });
-}
+  for (let i = 0; i < workerThreadsCount; i++)
+    workerThreads.push(new threads.Worker(__filename));
 
-for (let i = 0; i < options.count; i++) {
-  await generatePDF(`${i}.pdf`);
+  let readyDocsCount = 0;
+
+  function delegatePDFGeneration(worker) {
+    worker.postMessage({
+      outputDirectory,
+      fileName: `${readyDocsCount}.pdf`,
+      minParagraphs: options.minParagraphs,
+      maxParagraphs: options.maxParagraphs,
+    });
+    readyDocsCount++;
+  }
+
+  for (const worker of workerThreads) {
+    delegatePDFGeneration(worker);
+    worker.on("message", ({ done }) => {
+      if (readyDocsCount >= options.count) {
+        worker.terminate();
+      } else if (done) {
+        delegatePDFGeneration(worker);
+      }
+    });
+  }
+} else {
+  threads.parentPort?.on(
+    "message",
+    ({ outputDirectory, fileName, minParagraphs, maxParagraphs }) => {
+      const doc = new PDFDocument();
+      const stream = fs.createWriteStream(
+        path.resolve(outputDirectory, fileName)
+      );
+      doc.pipe(stream);
+      doc.text(
+        faker.lorem.paragraphs(
+          { min: minParagraphs, max: maxParagraphs },
+          "\n\n"
+        )
+      );
+      doc.end();
+      stream.on("close", () => {
+        threads.parentPort?.postMessage({ done: true });
+      });
+    }
+  );
 }
