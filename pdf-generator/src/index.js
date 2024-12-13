e1fd3fe2d6ed9a9beacc1171919cbebda535b1e1
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2024-12-13 18:40:48 +0300

    implement CLI options

--- pdf-generator/src/index.js
+++ pdf-generator/src/index.js
@@ -1,6 +1,18 @@
 import fs from "node:fs";
 import PDFDocument from "pdfkit";
 import { faker } from "@faker-js/faker";
+import { program } from "commander";
+
+program
+  .option("--seed <seed>", "random generation seed")
+  // @ts-ignore
+  .option("--count <count>", "pdf files count", 1)
+  // @ts-ignore
+  .option("--min-paragraphs <count>", "min paragraphs in pdf file", 10)
+  // @ts-ignore
+  .option("--max-paragraphs <count>", "max paragraphs in pdf file", 100);
+program.parse(process.argv);
+const options = program.opts();
 
 const doc = new PDFDocument();
 
